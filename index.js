a183535c97414c52249c0a6b33a76ec04bd109d7
DRMus <78694252+DRMus@users.noreply.github.com>
2022-09-29 00:56:01 +0400

    change questions and answers objects

--- index.js
+++ index.js
@@ -1,119 +1,138 @@
-const TelegramApi = require('node-telegram-bot-api');
+const TelegramApi = require("node-telegram-bot-api");
 
-const token = '5737427687:AAERsNNSHNsqJjrSmUtL2x-YAXnba49Utv0';
+const token = "5652422996:AAEcEdSQW4aEHIqtFt3npB7NZhraOfxc3qI";
 
 const bot = new TelegramApi(token, { polling: true });
 
 const questions = [
-    "\u{2753} Какую часть шаблона проектирования MVC реализует React.js?",
-    "\u{2753} Какой шаблонизатор можно использовать в React?",
-    "\u{2753} Перерисовывает ли React всё view когда изминелись props или состояние?",
-    "\u{2753} Какой метод компонента следует использовать для кастомной логики для реагирования на изменения?",
-    "\u{2753} Что можно передать как второй аргумент в метод setState?"
+  "\u{2753} К наиболее общим типам операндов нельзя отнести:",
+  "\u{2753} Какие признаки (флаги) формируются в АЛУ при выполнении арифметических и логических операций?",
+  "\u{2753} Верно ли утверждение, что достоинствами представления чисел в формате с фиксированной запятой являются простота и наглядность представления чисел, а также простота алгоритмов реализации арифметических операций?",
+  "\u{2753} Выберите прием, повышающий способ представления мантиссы в формате чисел с плавающей запятой:",
+  "\u{2753} Для всех языков программирования характерно интенсивное использование механизма.",
+  "\u{2753} Верно ли утверждение, что аббревиатура SIMD обозначает много данных – одна инструкция?",
 ];
 
 var result = 0;
 
 const answers = [
-    [
-        { text: "React - это полноценный MVC-фреймворк", validate: false },
-        { text: "React предназначен для работы с view частью", validate: true },
-        { text: "React является «контроллером» с точки зрения MVC", validate: false },
-    ],
-    [
-        { text: "HBS", validate: false },
-        { text: "EJS", validate: false },
-        { text: "XML", validate: false },
-        { text: "В React вы не должны использовать какой-либо шаблонизатор. Для рендеринга HTML используется JSX (расширение синтаксиса JS).", validate: true }
-    ],
-    [
-        { text: "Да. React.js распространяет событие на все дерево компонентов, и они вызывают метод render.", validate: false },
-        { text: "Нет. React только повторно рендерит только измененный компонент.", validate: true },
-    ],
-    [
-        { text: "shouldComponentUpdate", validate: false },
-        { text: "getSnapshotBeforeUpdate", validate: false },
-        { text: "componentWillUpdate", validate: true },
-        { text: "componentWillMount", validate: false },
-    ],
-    [
-        { text: "Cвойства, которые должны быть обновлены", validate: false },
-        { text: "Функцую обратного вызова для получения нового состояния", validate: false },
-        { text: "Предыдущее состояние", validate: true },
-    ]
+  [
+    { text: "Адреса", validate: false },
+    { text: "Ссылки", validate: true },
+    { text: "Числа", validate: false },
+    { text: "Символы", validate: false },
+  ],
+  [
+    { text: "Флаг нуля", validate: false },
+    { text: "флаг переполнения", validate: false },
+    { text: "флаг знака", validate: false },
+    { text: "флаг переноса", validate: false },
+    { text: "все перечисленные выше", validate: true },
+  ],
+  [
+    {
+      text: "Верно",
+      validate: true,
+    },
+    {
+      text: "Неверно",
+      validate: false,
+    },
+  ],
+  [
+    { text: "прием скрытой единицы", validate: true },
+    { text: "нормализации мантиссы справа", validate: false },
+    { text: "нормализация мантиссы слева", validate: false },
+  ],
+  [
+    { text: "процедур", validate: true },
+    {
+      text: "сортировки",
+      validate: false,
+    },
+    { text: "классов", validate: false },
+    { text: "функций", validate: false },
+  ],
+  [
+    { text: "Верно", validate: false },
+    { text: "Неверно", validate: true },
+  ]
 ];
 var indexQuest = 0;
 
 const startTest = async (chatId, right) => {
-    if (right) {
-        await bot.sendMessage(chatId, "Верно");
-    }
-    await bot.sendMessage(chatId, questions[indexQuest]);
+  if (right) {
+    await bot.sendMessage(chatId, "Верно");
+  }
+  await bot.sendMessage(chatId, questions[indexQuest]);
 
-    var answersForQuestions = [];
-    answers[indexQuest].map((a) => {
-        answersForQuestions.push([
-            { text: a.text, callback_data: a.validate.toString() },
-        ]);
-    });
+  var answersForQuestions = [];
+  answers[indexQuest].map((a) => {
+    answersForQuestions.push([
+      { text: a.text, callback_data: a.validate.toString() },
+    ]);
+  });
 
-    const gameOptions = {
-        reply_markup: JSON.stringify({
-            inline_keyboard: answersForQuestions,
-        }),
-    };
-    return await bot.sendMessage(chatId, "Выберите ответ", gameOptions);
+  const gameOptions = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: answersForQuestions,
+    }),
+  };
+  return await bot.sendMessage(chatId, "Выберите ответ", gameOptions);
 };
 
 const start = async () => {
-    bot.setMyCommands([
-        { command: "/start", description: "Начальное приветствие" },
-        { command: "/info", description: "Получить информацию о пользователе" },
-        { command: "/testing", description: "Тестирование по дисциплинам" },
-    ]);
+  bot.setMyCommands([
+    { command: "/start", description: "Начальное приветствие" },
+    { command: "/info", description: "Получить информацию о пользователе" },
+    { command: "/testing", description: "Тестирование по дисциплинам" },
+  ]);
 
-    bot.on("message", async (msg) => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
 
-        try {
-            if (text === "/start") {
-                return bot.sendMessage(
-                    chatId,
-                    `Добро пожаловать в бот, который тестирует ваши знания по React`
-                );
-            }
-            if (text === "/info") {
-                return bot.sendMessage(
-                    chatId,
-                    `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`
-                );
-            }
-            if (text === "/testing") {
-                return startTest(chatId, indexQuest);
-            }
-            return bot.sendMessage(chatId, "Я тебя не понимаю, попробуй еще раз!)");
-        } catch (e) {
-            return bot.sendMessage(chatId, `Произошла ошибка ${e}`);
-        }
-    });
+    try {
+      if (text === "/start") {
+        return bot.sendMessage(
+          chatId,
+          `Добро пожаловать в бот, который протестирует ваши знание ЭВМ`
+        );
+      }
+      if (text === "/info") {
+        return bot.sendMessage(
+          chatId,
+          `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`
+        );
+      }
+      if (text === "/testing") {
+        return startTest(chatId, indexQuest);
+      }
+      return bot.sendMessage(chatId, "Я тебя не понимаю, попробуй еще раз!)");
+    } catch (e) {
+      return bot.sendMessage(chatId, `Произошла ошибка ${e}`);
+    }
+  });
 
-    bot.on("callback_query", async (msg) => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-        if (indexQuest + 1 < questions.length) {
-            indexQuest += 1;
-            if (data === "true") {
-                result += 1;
-                return startTest(chatId, true);
-            } else {
-                return startTest(chatId);
-            }
-        } else {
-            await bot.sendMessage(chatId, `\u{2714} Вы ответили верно на ${result} вопросов из ${questions.length}`);
-            indexQuest = 0;
-        }
-    });
+  bot.on("callback_query", async (msg) => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+    if (indexQuest + 1 < questions.length) {
+      indexQuest += 1;
+      if (data === "true") {
+        result += 1;
+        return startTest(chatId, true);
+      } else {
+        return startTest(chatId);
+      }
+    } else {
+      await bot.sendMessage(
+        chatId,
+        `\u{2714} Вы ответили верно на ${result} вопросов из ${questions.length}`
+      );
+      indexQuest = 0;
+    }
+  });
 };
 
 start();
